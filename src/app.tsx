78d99c2093d25ab245f7ace7f1fd52f5308f8787
Cheng Ye <Cheng.Ye.HL@hotmail.com>
2021-03-31 09:39:15 +0800

    add copy function

--- src/app.tsx
+++ src/app.tsx
@@ -1,111 +1,189 @@
 // global CSS
-import globalCss from "./style.css";
+import globalCss from './style.css'
 // CSS modules
-import styles, { stylesheet } from "./style.module.css";
+import styles, { stylesheet } from './style.module.css'
 
-let inProgress = false;
+let inProgress = false
 
 function shortWait() {
   return new Promise((resolve) => {
-    let waitTime = 200;
+    let waitTime = 200
     setTimeout(() => {
-      resolve(waitTime);
-    }, waitTime);
-  });
+      resolve(waitTime)
+    }, waitTime)
+  })
 }
 
 export async function init() {
-  console.log("preparing CrowdinCrawler...");
+  console.log('preparing CrowdinCrawler...')
 
-  let toolbarSelector = "#editor-header .pull-right > .btn-toolbar";
+  let toolbarSelector = '#editor-header .pull-right > .btn-toolbar'
 
   while (document.querySelector(toolbarSelector) == null) {
-    await shortWait();
+    await shortWait()
   }
 
   let $crawlerButton = $(
-    '<button id="action-crawler" title="Translation Crawler" class="btn btn-icon"></button>'
-  );
+    '<button id="action-crawler" title="Translation Crawler" class="btn btn-icon"></button>',
+  )
 
-  $crawlerButton.append('<i class="static-icon-task"></i>');
-  $(toolbarSelector).append($crawlerButton);
+  $crawlerButton.append('<i class="static-icon-task"></i>')
+  $(toolbarSelector).append($crawlerButton)
 
-  $("#action-crawler").on("click", () => {
-    runCrawlerTask();
-  });
+  bindAction()
+}
+
+function bindAction() {
+  $('body').on('click', '#action-crawler', () => {
+    runCrawlerTask()
+  })
+
+  $('body').on('copy', '#copy-container', (event) => {
+    const selection = getSelection()
+    if (selection.isCollapsed) return
+    event.preventDefault()
+    const dataAsText = window.getSelection().toString()
+    const dataAsHtml = $('<div></div>')
+      .append(selection.getRangeAt(0).cloneContents())
+      .html()
+    const clipboardData = event.originalEvent['clipboardData']
+    clipboardData.setData('text/plain', dataAsText)
+    clipboardData.setData('text/html', brTagCopyFix(dataAsHtml))
+  })
+
+  $('body').on('click', '#translation-data-close', () => {
+    $('#translation-data-panel').remove()
+  })
+
+  $('body').on('click', '#action-copy-table', () => {
+    copyFormatted($('#translate-data-container').html())
+  })
+}
+
+function copyFormatted(html) {
+  // Create container for the HTML
+  // [1]
+  var container = document.createElement('div')
+  container.innerHTML = html
+
+  // Hide element
+  // [2]
+  container.id = 'copy-container'
+  container.style.position = 'fixed'
+  container.style.pointerEvents = 'none'
+  container.style.opacity = '0'
+
+  // Detect all style sheets of the page
+  var activeSheets = Array.prototype.slice
+    .call(document.styleSheets)
+    .filter(function (sheet) {
+      return !sheet.disabled
+    })
+
+  // Mount the container to the DOM to make `contentWindow` available
+  // [3]
+  document.body.appendChild(container)
+
+  // Copy to clipboard
+  // [4]
+  window.getSelection().removeAllRanges()
+
+  var range = document.createRange()
+  range.selectNode(container)
+  window.getSelection().addRange(range)
+
+  // [5.1]
+  document.execCommand('copy')
+
+  // [5.2]
+  for (var i = 0; i < activeSheets.length; i++) activeSheets[i].disabled = true
+
+  // [5.3]
+  document.execCommand('copy')
+
+  // [5.4]
+  for (var i = 0; i < activeSheets.length; i++) activeSheets[i].disabled = false
+
+  // Remove the container
+  // [6]
+  document.body.removeChild(container)
 }
 
 async function runCrawlerTask() {
   if (inProgress == true) {
-    return;
+    return
   }
-  console.log("start working...");
-  inProgress = true;
-  $("#action-crawler").prop("disabled", true);
-  let transData = [];
-  let hasMoreJob = true;
+  console.log('start working...')
+  inProgress = true
+  $('#action-crawler').prop('disabled', true)
+  let transData = []
+  let hasMoreJob = true
 
   while (hasMoreJob) {
-    if ($("#next_page").prop("disabled")) {
-      console.log("last page...");
-      await listJob(transData);
-      printResult(transData);
-      hasMoreJob = false;
+    if ($('#next_page').prop('disabled')) {
+      console.log('last page...')
+      await listJob(transData)
+      printResult(transData)
+      hasMoreJob = false
     } else {
-      await listJob(transData);
-      $("#next_page").trigger("click");
-      await shortWait();
-      while ($("#loading_block").css("display") != "none") {
-        await shortWait();
+      await listJob(transData)
+      $('#next_page').trigger('click')
+      await shortWait()
+      while ($('#loading_block').css('display') != 'none') {
+        await shortWait()
       }
     }
   }
 
-  inProgress = false;
-  $("#action-crawler").prop("disabled", false);
+  inProgress = false
+  $('#action-crawler').prop('disabled', false)
 }
 
 async function listJob(transData) {
-  console.log("doing jobs...");
-  let textListLength = $("#texts_list li").length;
+  console.log('doing jobs...')
+  let textListLength = $('#texts_list li').length
   for (let index = 0; index < textListLength; index++) {
-    $("#texts_list li").eq(index).trigger("mousedown");
+    $('#texts_list li').eq(index).trigger('mousedown')
     for (let i = 0; i < 10; i++) {
-      await shortWait();
+      await shortWait()
       if (
-        $("#source_phrase_container").text() ==
-        $("#texts_list li").eq(index).text()
+        $('#source_phrase_container').text() ==
+        $('#texts_list li').eq(index).text()
       ) {
         transData.push([
-          $("#source_context_container > div").text(),
-          $("#texts_list li").eq(index).text(),
-          $("#translation").val(),
-        ]);
-        break;
+          $('#source_context_container > div').text(),
+          $('#texts_list li').eq(index).text(),
+          $('#translation').val().toString(),
+        ])
+        break
       }
     }
   }
 }
 
 function printResult(data) {
-  $("body").append(resultTable(data));
-  $("#translate-data-container")
+  $('body').append(resultTable(data))
+  $('#translate-data-container')
     .contents()
     .filter((_, elem) => {
-      return elem.nodeType == Node.TEXT_NODE;
+      return elem.nodeType == Node.TEXT_NODE
     })
-    .remove();
-  $("#translation-data-close").on("click", () => {
-    $("#translation-data-panel").remove();
-  });
-  console.log(data);
+    .remove()
 }
 
-function resultTable(tableContent) {
+function brTagCopyFix(htmlContent: String) {
+  const lineBreakRegex = /\r\n|\r|\n/g
+  return htmlContent.replace(
+    lineBreakRegex,
+    '<br style="mso-data-placement:same-cell;" />',
+  )
+}
+
+function resultTable(tableContent: Array<Array<String>>) {
   return (
     <>
       <div
-        class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-draggable ui-dialog-buttons"
+        class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-dialog-buttons"
         id="translation-data-panel"
         tabindex="-1"
         role="dialog"
@@ -115,6 +193,14 @@ function resultTable(tableContent) {
           <span id="ui-id-9999" class="ui-dialog-title">
             Result
           </span>
+          <button
+            id="action-copy-table"
+            tabindex="-1"
+            title="Copy Table to Clipboard"
+            class="btn btn-icon ui-dialog-titlebar-copy"
+          >
+            <i class="static-icon-copy-source"></i>
+          </button>
           <a
             href="#"
             class="ui-dialog-titlebar-close ui-corner-all"
@@ -142,13 +228,13 @@ function resultTable(tableContent) {
                 <th>Source</th>
                 <th>Translation</th>
               </tr>
-              {tableContent.map((row, i) =>
-              <tr key={i}>
-                <td>{row[0]}</td>
-                <td>{row[1]}</td>
-                <td>{row[2]}</td>
-              </tr>
-              )}
+              {tableContent.map((row, i) => (
+                <tr key={i}>
+                  <td>{row[0]}</td>
+                  <td>{row[1]}</td>
+                  <td>{row[2]}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -156,5 +242,5 @@ function resultTable(tableContent) {
       <style>{globalCss}</style>
       <style>{stylesheet}</style>
     </>
-  );
+  )
 }
